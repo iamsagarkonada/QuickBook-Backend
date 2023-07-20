c22af740943d98f77b707470216f71427e4af6d6
iamsagarkonada <sagar.konda3@gmail.com>
2023-07-20 16:23:40 +0530

    Update Mongo

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 const mongoose=require('mongoose');
-
-const mongoURI= "mongodb://127.0.0.1:27017/inotebook"
-const connectMongo=()=>{
-    mongoose.connect(mongoURI)
-        console.log("Connect to mongo Sucessfully");
-}
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoURI= process.env.MONGO_CONNECT_URL
+const connectMongo=async()=>{
+    try {
+        await mongoose.connect(mongoURI),
+        console.log("Connect to mongo Server Sucessfully")
+    }catch (error) {
+        console.log("Connection Failed "+error)
+    }
+    } 
+    
 module.exports=connectMongo;
